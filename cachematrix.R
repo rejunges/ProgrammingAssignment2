03a53fd4930cbc74b0aa21cba17a9f2b3e0dc6a9
Renata <rzjunges@inf.ufpel.edu.br>
2018-05-31 22:33:09 -0300

    Coursera week 3 - assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,20 @@
 ## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
     inverse <- NULL
+    
+    #set a new matrix
     set <- function(y){
         x <<- y
         inverse <<- NULL
     }
+    
+    #get the current matrix
     get <- function() x
+    
+    #set inverse of matrix
     setInverse <- function(inv) inverse <<- inv
+    
+    #get the inverse of matrix
     getInverse <- function() inverse
     list(set = set, get = get,
          setInverse = setInverse,
@@ -21,10 +29,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
     inverse <- x$getInverse()
+    
+    #verify if the inverse of matrix has been calculated
     if(!is.null(inverse)){
         message("getting cached data")
-        return(inverse)
+        return(inverse) #return the inverse
     }
+    
+    #If the inverse has not been calculated then get the matrix
+    #calculated the inverse, set the inverse and return the inverse
     data <- x$get()
     inverse <- solve(data,...)
     x$setInverse(inverse)
